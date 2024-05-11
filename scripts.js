0550ae9eb240572a7d477d4c9229d8e3117792bb
and-is <andispaudel@gmail.com>
2024-05-11 18:38:04 +0545

    modified all and implemented whatever that's needed

--- scripts.js
+++ scripts.js
@@ -1,15 +1,41 @@
-const gameBoard = () => {
-  const row = 3;
-  const column = 3;
-  const board = {};
-  for (let i = 0; i < row * column; i++) {
-    board[i + 1] = "";
-  }
-  return board;
-};
+function Player(name, icon) {
+  this.name = name;
+  this.icon = icon;
+  this.turn = true;
+  this.toggleTurn = () => {
+    this.turn = !this.turn;
+  };
+}
 
-const updateBoard = (board, input, icon) => {
-  board[input] = icon;
+const gameBoard = () => {
+  let board = {};
+  const player1 = new Player("Ram", "X");
+  const player2 = new Player("Shyam", "O");
+  let a = false;
+  player2.toggleTurn();
+  for (let i = 1; i < 10; i++) {
+    board[i] = "";
+    const bhaado = document.querySelector(`#a${i}`);
+    bhaado.addEventListener("click", () => {
+      if (!a) {
+        if (board[i] === "") {
+          if (player1.turn) {
+            bhaado.innerText = player1.icon;
+            board[i] = player1.icon;
+            console.log(board);
+            a = checkWin(board, player1, player2);
+          } else if (player2.turn) {
+            bhaado.innerText = player2.icon;
+            board[i] = player2.icon;
+            console.log(board);
+            a = checkWin(board, player1, player2);
+          }
+          player2.toggleTurn();
+          player1.toggleTurn();
+        }
+      }
+    });
+  }
 };
 
 const checkWin = (gameBoardState, player1, player2) => {
@@ -34,11 +60,14 @@ const checkWin = (gameBoardState, player1, player2) => {
       gameBoardState[9] === "X") ||
     (gameBoardState[1] === gameBoardState[5] &&
       gameBoardState[5] === gameBoardState[9] &&
-      gameBoardState[9] === "X")
+      gameBoardState[9] === "X") ||
+    (gameBoardState[3] === gameBoardState[5] &&
+      gameBoardState[5] === gameBoardState[7] &&
+      gameBoardState[7] === "X")
   ) {
-    console.log("Game Over");
-    console.log(player1.name + " wins!");
-    return false;
+    const bhaado2 = document.querySelector(".declaration");
+    bhaado2.textContent = player1.name + " wins!";
+    return true;
   } else if (
     (gameBoardState[1] === gameBoardState[2] &&
       gameBoardState[2] === gameBoardState[3] &&
@@ -60,54 +89,21 @@ const checkWin = (gameBoardState, player1, player2) => {
       gameBoardState[9] === "O") ||
     (gameBoardState[1] === gameBoardState[5] &&
       gameBoardState[5] === gameBoardState[9] &&
-      gameBoardState[9] === "O")
+      gameBoardState[9] === "O") ||
+    (gameBoardState[3] === gameBoardState[5] &&
+      gameBoardState[5] === gameBoardState[7] &&
+      gameBoardState[7] === "O")
   ) {
-    console.log("Game Over");
-    console.log(player2.name + " wins!");
-    return false;
-  } else if (Object.values(gameBoardState).every((x) => x !== "")) {
-    console.log("Draw");
-    return false;
-  } else {
+    const bhaado2 = document.querySelector(".declaration");
+    bhaado2.textContent = player2.name + " wins!";
     return true;
+  } else if (Object.values(gameBoardState).every((x) => x !== "")) {
+    const bhaado2 = document.querySelector(".declaration");
+    bhaado2.textContent = "Draw";
+    return true;
+  } else {
+    return false;
   }
 };
-function player(name, icon) {
-  this.name = name;
-  this.icon = icon;
-}
-const playGame = () => {
-  const player1 = new player("Ram", "X");
-  const player2 = new player("Shyam", "O");
 
-  let gameBoardState = gameBoard();
-
-  const takeInput = (player) => {
-    let input = prompt(
-      "Enter number, you're " + player.name + " Icon: " + player.icon
-    );
-    input = parseInt(input);
-    while (true) {
-      if (!gameBoardState[input] && !(input < 1) && !(input > 9) && input) {
-        updateBoard(gameBoardState, input, player.icon);
-        break;
-      } else {
-        console.log("Invalid");
-        input = input = prompt(
-          "Enter number, you're " + player.name + " Icon: " + player.icon
-        );
-      }
-    }
-  };
-
-  while (true) {
-    console.log(gameBoardState);
-    takeInput(player1);
-    console.log(gameBoardState);
-    if (!checkWin(gameBoardState, player1, player2)) break;
-    takeInput(player2);
-    console.log(gameBoardState);
-    if (!checkWin(gameBoardState, player1, player2)) break;
-  }
-};
-playGame();
+gameBoard();
